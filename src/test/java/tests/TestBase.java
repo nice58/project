edb7e13cb0689d9a77123ce267f40f422d8a7450
n.trubanenko <n.trubanenko@evotor.ru>
2023-10-25 00:17:18 +0300

    correction

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,5 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -10,11 +9,13 @@ import org.junit.jupiter.api.BeforeEach;
 import pages.*;
 import pages.components.AttachmentFile;
 
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.Map;
+import config.ConfigReader;
+import config.WebDriverConfig;
+import config.WebDriverProvider;
 
 public class TestBase {
+
+    private static final WebDriverConfig config = ConfigReader.Instance.read();
 
     MainPage mainPage = new MainPage();
     CatalogPage catalogPage = new CatalogPage();
@@ -22,24 +23,11 @@ public class TestBase {
 
 
     @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://www.wildberries.ru/";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.pageLoadStrategy = "eager";
+    public static void beforeAll() {
+        WebDriverProvider webDriverProvider = new WebDriverProvider(config);
+        webDriverProvider.webDriverConfig();
 
-        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-
-
-        Configuration.browserCapabilities = capabilities;
-    }
+}
 
     @BeforeEach
     void addListener() {
